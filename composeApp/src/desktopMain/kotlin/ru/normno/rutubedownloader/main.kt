e6c5455c221cef0a2edc1d05a883506ec1118c6f
NORMss <serega.b301@gmail.com>
2025-05-29 02:35:59 +0700

    init project

---
+++ composeApp/src/desktopMain/kotlin/ru/normno/rutubedownloader/main.kt
@@ -0,0 +1,13 @@
+package ru.normno.rutubedownloader
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "rutubeDownloader",
+    ) {
+        App()
+    }
+}
